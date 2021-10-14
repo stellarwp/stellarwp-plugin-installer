f51cb801e67f4a158e0acb9901c8e1278093024c
Norcross <andrew.norcross@gmail.com>
2021-10-14 16:00:17 -0400

    removing initial scaffolding that wasn't needed

--- stellarwp-plugin-installer.php
+++ stellarwp-plugin-installer.php
@@ -33,10 +33,8 @@ define( __NAMESPACE__ . '\FILE', __FILE__ );
 define( __NAMESPACE__ . '\PLUGIN', plugin_basename( __FILE__ ) );
 
 // Define the various prefixes we use.
-define( __NAMESPACE__ . '\OPTION_PREFIX', 'swp_installer_data_' );
 define( __NAMESPACE__ . '\CACHE_PREFIX', 'swp_pi_cache_' );
 define( __NAMESPACE__ . '\HOOK_PREFIX', 'swp_installer_' );
-define( __NAMESPACE__ . '\NONCE_PREFIX', 'swp_pi_nonce_' );
 
 // And load our files.
 stellarwp_plugin_installer_file_load();
@@ -49,11 +47,6 @@ stellarwp_plugin_installer_file_load();
 function stellarwp_plugin_installer_file_load() {
 
 	// Pull in the individual admin pieces.
-	require_once __DIR__ . '/includes/admin/data.php';
-	require_once __DIR__ . '/includes/admin/tabs.php';
-
-	// And last, load the activations and whatnot.
-	require_once __DIR__ . '/includes/activate.php';
-	require_once __DIR__ . '/includes/deactivate.php';
-	require_once __DIR__ . '/includes/uninstall.php';
+	require_once __DIR__ . '/includes/data.php';
+	require_once __DIR__ . '/includes/tabs.php';
 }
